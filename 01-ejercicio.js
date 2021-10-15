a966987795dbf5f84b2df847ac01be5c9212c743
Mercado Facundo <mercadofaundo21@gmail.com>
2021-10-15 14:02:06 -0300

    Primer codigo subido

---
+++ 01-ejercicio.js
@@ -0,0 +1,21 @@
+/*1. crea una variable con tu nombre
+2. crea una variable con tu apellido
+3. crea una variable con tu edad
+4. mostralo en pantalla todos esos datos concatenados
+5. sumale un numero a la edad
+6. mostralo en pantalla todos esos datos concatenados
+7. agrega comentarios en cada una de las líneas explicando lo que hacen 
+8. subilo a tu repositorio*/
+
+//nombre
+let name = 'Facundo'
+//apellido
+let lastName = 'Mercado'
+//edad
+let age = 26
+//imprimo en consola
+console.log(`${name} ${lastName} ${age}`)
+//sumo a la edad
+age = age + 1
+//vuelvo a mostrar
+console.log(age)
